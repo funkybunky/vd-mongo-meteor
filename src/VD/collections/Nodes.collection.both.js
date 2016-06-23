84697c2bf05cca6fda77a75152fd4849ac92f60a
Marcus Kleppe <marcuskleppe@gmail.com>
2016-06-23 18:26:43 +0200

    add collection2

--- src/VD/collections/Nodes.collection.both.js
+++ src/VD/collections/Nodes.collection.both.js
@@ -2,4 +2,14 @@ import { Mongo } from 'meteor/mongo'
 
 const Nodes = new Mongo.Collection('nodes')
 
+Nodes.schema = new SimpleSchema({
+  title: {type: String},
+  children: {
+    type: [String],
+    regEx: SimpleSchema.RegEx.Id,
+    optional: true,
+    label: 'array of link IDs'
+  }
+});
+
 export default Nodes
